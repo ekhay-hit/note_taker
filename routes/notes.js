0d8c8f6811ba93087bd21ab05188ec21f51af853
Houssine <ekhay1@outlook.com>
2024-07-04 13:54:28 -0400

    added comments to the logic and removed debuging console

--- routes/notes.js
+++ routes/notes.js
@@ -1,22 +1,17 @@
+// requiring router from express
 const router = require("express").Router();
 const fs = require("fs");
-const path = require("path");
-const db =require('../db/db.json')
 const uuid_Random = require("../helpers/uuid");
 
-
-
+// post request to add new notes 
 router.post('/notes', (req, res)=>{
 
-    // distructuring object 
-    console.log('I am in first line in post ');
-    console.log(req.body);
-
+    // distracturing the object 
     const {title, text} = req.body;
 
     res.json(`${req.method} recieved`)
-    // console.info(`I am in ${req.method} title is: ${title} content: ${text}`);
-
+  
+    // creating the new note
     if(title && text){
         const newNote ={
             title,
@@ -24,9 +19,7 @@ router.post('/notes', (req, res)=>{
             id: uuid_Random(),
         };
 
-        //convert data to string for storage
-        // const notes = JSON.stringify(newNote);
-      
+      // reading data from db.json
         fs.readFile("./db/db.json",'utf8',(err,data)=>{
             if (err){
 
@@ -57,25 +50,31 @@ router.post('/notes', (req, res)=>{
       
 })
 
-//
+// Delet Post request
 router.delete('/notes/:id',(req, res)=>{
-    console.log('I entered delete');
+   
+    // distructing the object of the url to pull the id
    const { params: {id}}  = req;
     
-    console.log(id);
-   // read the file
+   // read the file json
    fs.readFile("./db/db.json",'utf8', (error, data)=>{
     if(error){
      console.log(error);
     }else{
+
+        // parsing the data in the file of db.json
         const parsedNoteForDelete= JSON.parse(data);
 
+        // finding the index of the note base on the id requested to delete 
         const findNoteIndex = parsedNoteForDelete.findIndex((note) => note.id === id);
-        if(findNoteIndex === -1) return res.sendStatus(404);
-        console.log(findNoteIndex);
-        parsedNoteForDelete.splice(findNoteIndex,1);
-        console.log('I am here in delete');
         
+        // if no id send bad status of 404, not found
+        if(findNoteIndex === -1) return res.sendStatus(404);
+        
+        // after finding the index, delete the not from the file
+        parsedNoteForDelete.splice(findNoteIndex,1);
+       
+        // write the data back to the db.json after deleting the requested note.
         fs.writeFile("./db/db.json", JSON.stringify(parsedNoteForDelete,null,4),(error) =>{
             error? console.error("ERORR data failed to write"):console.info('Note has been deleted');
         })
